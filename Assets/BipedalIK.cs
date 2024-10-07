2ac645ee595c60a52896cb9a8e31cb628d17a72e
Brymir7 <Brymir7@gmail.com>
2024-10-07 13:06:33 +0200

    reuse old IK

--- Assets/BipedalIK.cs
+++ Assets/BipedalIK.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -7,6 +6,7 @@ public class BipedalIK : MonoBehaviour
     public GameObject leftLeg;
     public GameObject rightLeg;
     public bool animateLegs;
+    public float epsilonForSameLoop = 0.1f;
     private IK _leftLegIK;
     private IK _rightLegIK;
     private Vector3 _velocity = Vector3.zero;
@@ -16,8 +16,6 @@ public class BipedalIK : MonoBehaviour
     private float _rightLegAnimationTime;
     [SerializeField] private float stepDistance;
     [SerializeField] private float animationTime = 0.5f;
-    [SerializeField] private List<Transform> feetKeyFramesWalking;
-    [SerializeField] private List<Transform> feetKeyFramesRunning;
 
     public delegate void OnLegHitGroundCallback();
 
@@ -66,24 +64,58 @@ public class BipedalIK : MonoBehaviour
             return;
         }
 
-        _leftLegAnimationTime = (_leftLegAnimationTime + Time.deltaTime) % animationTime;
-        _rightLegAnimationTime = (_rightLegAnimationTime + Time.deltaTime) % animationTime;
-        _leftLegIK.target = CalculateFootTarget(_leftLegIK, _leftLegAnimationTime, 1);
-        _rightLegIK.target = CalculateFootTarget(_rightLegIK, _rightLegAnimationTime, -1);
+        var nextLegLeftPos = GetNextFootPosition(_leftLegIK);
+        if (
+            Vector3.Distance(nextLegLeftPos, _nextLeftLegTarget) > stepDistance * 2.0)
+        {
+            _nextLeftLegTarget = nextLegLeftPos;
+            _leftLegAnimationTime = 0.0f;
+        }
+
+        var nextLegRightPos = GetNextFootPosition(_rightLegIK);
+        // only move right feet if right is behind and left is in front by at least a step distance
+        var forwardDistanceRightFeetToLeftTarget =
+            Vector3.Dot(transform.forward, (nextLegRightPos - _leftLegIK.target));
+        // if legs got stuck || are in the same loop due to physics || initial position, then only move left leg
+        var sameLoop = Vector3.Distance(_rightLegIK.joints[_rightLegIK.joints.Count - 1].position,
+            _leftLegIK.joints[_leftLegIK.joints.Count - 1].position) < epsilonForSameLoop;
+        if (!sameLoop && forwardDistanceRightFeetToLeftTarget > stepDistance &&
+            Vector3.Distance(nextLegRightPos, _nextRightLegTarget) > stepDistance * 2.0f)
+        {
+            _nextRightLegTarget = nextLegRightPos;
+            _rightLegAnimationTime = 0.0f;
+        }
+
+        _leftLegIK.target =
+            Vector3.Lerp(_leftLegIK.target, _nextLeftLegTarget, _leftLegAnimationTime / animationTime);
+        _rightLegIK.target =
+            Vector3.Lerp(_rightLegIK.target, _nextRightLegTarget, _rightLegAnimationTime / animationTime);
+
+        if (_leftLegAnimationTime + Time.deltaTime > animationTime ||
+            _rightLegAnimationTime + Time.deltaTime > animationTime)
+        {
+            _onLegHitGroundCallback?.Invoke();
+        }
+
+        _leftLegAnimationTime += Time.deltaTime;
+        _rightLegAnimationTime += Time.deltaTime;
     }
 
-    private Vector3 CalculateFootTarget(IK legIK, float currAnimT, float dir)
+    Vector3 GetNextFootPosition(IK legIK)
     {
-        Vector3 footPosition = legIK.joints[0].position + transform.up * (-1 * legIK.totalBoneLength);
-        float stepProgress = Mathf.Sin(currAnimT / animationTime) * stepDistance;
+        Vector3 forward = transform.forward * stepDistance;
+        Vector3 stepPosition = legIK.joints[0].position + forward; // from hip forward
 
-        Vector3 target = stepProgress * dir * transform.forward + footPosition;
-        // if (Physics.Raycast(target + Vector3.up, Vector3.down, out RaycastHit hit))
-        // {
-        //     target = hit.point;
-        // }
+        if (Physics.Raycast(stepPosition + Vector3.up, Vector3.down, out RaycastHit hit))
+        {
+            if (hit.transform.gameObject.CompareTag("Ground"))
+            {
+                stepPosition = hit.point;
+                return stepPosition;
+            }
+        }
 
-        return target;
+        return legIK.joints[legIK.joints.Count - 1].position + forward;
     }
 
     Vector3 GetRestingFootPosition(IK legIK)
